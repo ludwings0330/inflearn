05aca3b09b85e2c06f3132fb8810c06c7d83f997
ludwings <ludwings0330@gmail.com>
2022-03-07 18:40:08 +0900

    [Spring MVC] 기타 내용 추가

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,9 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(name = "helloSer")
 @SpringBootApplication
 public class ServletApplication {
 
--- servlet/src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
+++ servlet/src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -1,2 +1,36 @@
-package hello.servlet.web.springmvc.v1;public class SpringMemberFormControllerV1 {
+package hello.servlet.web.springmvc.v1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@RequestMapping("/springmvc/v2/members")
+public class SpringMemberFormControllerV1 {
+
+    @RequestMapping("/new-form")
+    public ModelAndView process() {
+        return new ModelAndView("new-form");
+    }
+
+    @RequestMapping("/save")
+    public ModelAndView saveMember(HttpServletRequest request, HttpServletResponse response){
+        final ModelAndView modelAndView = new ModelAndView("save-result");
+
+        modelAndView.addObject("member", "member object");
+
+        return modelAndView;
+    }
+
+    @RequestMapping
+    public ModelAndView members() {
+        final ModelAndView mv = new ModelAndView("members");
+
+        mv.addObject("member", "members object");
+
+        return mv;
+    }
 }
